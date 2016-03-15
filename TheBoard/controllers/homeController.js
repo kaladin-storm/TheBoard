1f7086d6fc968603e86ab0fa0a8a0613722bc61c
kaladin-storm <openspacetest112013+kaladin@gmail.com>
2016-03-15 17:20:23 +0000

    Angular with bower

--- TheBoard/controllers/homeController.js
+++ TheBoard/controllers/homeController.js
@@ -17,6 +17,12 @@
 
             
         });
+        
+        app.get("/notes/:categoryName", function (req, res) {
+            var categoryName = req.params.categoryName;
+
+            res.render("notes", { title: categoryName });
+        });
 
         app.post("/newCategory", function (req, res) {
             var categoryName = "physics";
---
+++ TheBoard/public/js/notesView.js
@@ -0,0 +1,48 @@
+﻿//notesView.js
+
+(function (angular) {
+
+    var theModule = angular.module("notesView", ["ui.bootstrap"]);
+
+    theModule.controller("notesViewController", ["$scope", "$window", "$http",
+        function ($scope, $window, $http) {
+            $scope.notes = [];
+            $scope.newNote = createBlankNote();
+
+            //Get category name
+            var urlParts = $window.location.pathname.split("/");
+            var categoryName = urlParts[urlParts.length - 1];
+
+            var notesUrl = "/api/notes/" + categoryName;
+
+            //promises - chain several calls together
+            $http.get(notesUrl)
+                .then(function (result) {
+                    $scope.notes = result.data;
+                }, function (err) {
+                    alert(err);
+            });
+
+
+            $scope.save = function () {
+
+                $http.post(notesUrl, $scope.newNote)
+                .then(function (result) {
+                    $scope.notes.push(result.data);
+                    $scope.newNote = createBlankNote();
+                }, function (err) {
+                    //TODO
+                                
+                })
+            };
+        }
+    ]);
+
+    function createBlankNote() {
+        return {
+            note: "",
+                color: "yellow"
+            };
+    }
+
+})(window.angular);
